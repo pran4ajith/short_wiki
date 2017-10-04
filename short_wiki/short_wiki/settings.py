2bec619a06ed5b6aeeec6df270bb00889b6b0bd2
pranav <pran4ajith@gmail.com>
2017-10-04 15:15:23 +0000

    pyc

--- short_wiki/short_wiki/settings.py
+++ short_wiki/short_wiki/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1eu=^(q*#m32tfs1328%7@=w+!4_ue3%ofpdv@)n-8im%f604f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pran4ajith.pythonanywhere.com',]
 
 
 # Application definition
